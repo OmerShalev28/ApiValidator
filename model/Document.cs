f07629ae3bf54ab86490fc7cf82298c9a0d8a9c8
Omer Shalev <omer.shalev28@gmail.com>
2025-02-19 16:07:43 +0200

    created class Document

---
+++ model/Document.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiValidator.model
+{
+    public enum DocumentType
+    {
+        InvoiceReceipt,
+        Receipt, 
+        ReceiptRefund,
+        Invoice, 
+        RefundInvoice, 
+        DealInvoice, 
+        PriceProposal, 
+        OrderConfirmation, 
+        ShippingCertificate, 
+        DonationReceipt, 
+        ReturnCertificate
+    }
+    public class Document
+    {
+        public string ProviderUserToken { get; set; }
+        public string ProviderMsgId { get; set; }
+        public string ProviderMsgReferenceId { get; set; }
+        public DocumentType DocumentType { get; set; }
+        public Customer Customer { get; set; }
+        public string DocDate { get; set; }
+
+        public Document(string providerUserToken, string providerMsgId, string providerMsgReferenceId,
+                        DocumentType documentType, Customer customer, string docDate)
+        {
+            ProviderUserToken = providerUserToken;
+            ProviderMsgId = providerMsgId;
+            ProviderMsgReferenceId = providerMsgReferenceId;
+            DocumentType = documentType;
+            Customer = customer;
+            DocDate = docDate;
+        }
+    }
+}
