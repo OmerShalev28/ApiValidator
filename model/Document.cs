43bf65780079349ffc8cc25679920cd141e06a42
Omer Shalev <omer.shalev28@gmail.com>
2025-02-19 16:44:22 +0200

    document generating works

--- model/Document.cs
+++ model/Document.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 namespace ApiValidator.model
 {
@@ -21,13 +22,25 @@ namespace ApiValidator.model
     }
     public class Document
     {
+        [JsonPropertyName("providerUserToken")]
         public string ProviderUserToken { get; set; }
+
+        [JsonPropertyName("providerMsgId")]
         public string ProviderMsgId { get; set; }
+
+        [JsonPropertyName("providerMsgReferenceId")]
         public string ProviderMsgReferenceId { get; set; }
+
+        [JsonPropertyName("docType")]
         public DocumentType DocumentType { get; set; }
+
+        [JsonPropertyName("customer")]
         public Customer Customer { get; set; }
+
+        [JsonPropertyName("docDate")]
         public string DocDate { get; set; }
 
+        [JsonConstructor]
         public Document(string providerUserToken, string providerMsgId, string providerMsgReferenceId,
                         DocumentType documentType, Customer customer, string docDate)
         {
